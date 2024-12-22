cd4374e9a299bfae36c7c3563b827dd52f61711a
Chandra Pratap shah <136089044+chandrapratapshah@users.noreply.github.com>
2024-12-22 10:19:09 +0530

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,134 @@
+# name = "Chandra" #string
+# age  = 21 #Integer
+# price = 25.99 #float
+# age2 = age
+
+# print("My name is :", name)
+# print("My age is :", age2)
+# print("My price is :", price)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# name1 = "ch"
+# name2 = 'ch'
+# name3 = '''ch'''
+
+# print(name1)
+# print(name2)
+# print(name3)
+
+# '''Booleam and None Data Type'''
+
+# age = 21
+# old = True
+# a = None
+# print(type(old))
+# print(type(a))
+
+# #Print Sum
+# """
+# Print
+# Sum
+# """
+# a = 2000
+# b = 500
+# diff = a - b
+# print(diff)
+
+#Arithmetic Operators
+
+# a = 5
+# b = 2
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b) #Remainder
+# print(a ** b) #a^b
+
+#Relation Operator
+# a = 50
+# b = 40
+
+# print(a == b) #False
+# print(a != b) #True
+# print(a >= b) #True
+# print(a <= b) #False
+# print(a < b) #False
+
+#Assignment Operator
+
+# num = 10
+# # num = num + 10
+# num **= 10
+# print("num :", num) #20 and if diff then 0
+
+#Logical Operator 
+# a = 30
+# b = 10
+# print(not False) #True
+# print(not (a > b)) #False
+
+# val1 = True
+# val2 = False
+# print("AND operator:", (a == b) and (a > b))
+# print("OR operator:", (a == b) or (a > b))
+
+#Type Conversion
+
+# a = "2" #string So, str to float is not possible to convert,
+''' You can convert Int to Float '''
+# b = 4.25
+# sum = a + b
+# print(sum)
+
+#Type Casting
+
+# a = float("2")
+# b = 4.25
+
+# print(a + b)
+# print(type(a))
+
+
+# a = 3.4
+# a = str(a)
+# print(type(a))
+
+'''Input in Python'''
+# name = input("Enter your age:")
+# print("you entered", name)
+
+# val = int(input("enter dome value:"))
+# print(type(val), val)
+
+# name = input("Enter your name:")
+# age = int(input("Enter you age:"))
+# marks = float(input("Emter Marks:"))
+
+# print("Welcome,", name)
+# print("Age =", age)
+# print("Marks =", marks)
+
+'''Let's Practice'''
+#Q1
+# first_num = int(input("Enter First Number:"))
+# second_num = int(input("Enter Second Number:"))
+# print("Sum = ", first_num + second_num)
+
+#Q2
+# a = int(input("Enter side of Square:"))
+# print("Area =", a**2,"Sq Unit")
+
+#Q3
+# a = float(input("Enter Decimals First Number"))
+# b = float(input("Enter Decimals Second Number"))
+# print("Average =", (a + b)/2)
+
+#Q4
+# a = int(input("Enter Number:"))
+# b = int(input("Enter Number:"))
+# print(a >= b)
